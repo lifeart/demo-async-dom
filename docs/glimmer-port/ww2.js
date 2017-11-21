e2d5d5d79fd12102b86681ea50c5fce34d6761e4
lifeart <lifeart92@gmail.com>
2017-11-21 05:47:04 +0300

    +

--- docs/glimmer-port/ww2.js
+++ docs/glimmer-port/ww2.js
@@ -37,7 +37,7 @@ window.setTimeout = self.setTimeout;
 // };
 
 
-self.importScripts('/ww/app.js');
+self.importScripts('ww/app.js');
 // self.importScripts('https://emberobserver.com/assets/vendor-63fb928166ab18a0d8a59b4c0239d88b.js');
 var regeneratorRuntime = window.regeneratorRuntime;
 // self.importScripts('https://emberobserver.com/assets/ember-observer-6183d2e97f093c7a7b31ab8faeda96d6.js');
