4b2a50a9c637f3b3c118a8c2867a87deff91453a
lifeart <lifeart92@gmail.com>
2017-11-21 22:02:44 +0300

    links update

--- docs/ember/ww2.js
+++ docs/ember/ww2.js
@@ -34,10 +34,9 @@ window.setTimeout = self.setTimeout;
 //   },time);
 // };
 
-
-self.importScripts('https://ember-api-docs-frontend.global.ssl.fastly.net/assets/vendor-be4c74c653b439444054fbaf16b02bea.js');
+self.importScripts('https://ember-api-docs-frontend-staging.global.ssl.fastly.net/assets/vendor-0b337e88c5b7a635a3281ba1f12d6048.js');
 // self.importScripts('https://emberobserver.com/assets/vendor-63fb928166ab18a0d8a59b4c0239d88b.js');
 var regeneratorRuntime = window.regeneratorRuntime;
 // self.importScripts('https://emberobserver.com/assets/ember-observer-6183d2e97f093c7a7b31ab8faeda96d6.js');
-self.importScripts('https://ember-api-docs-frontend.global.ssl.fastly.net/assets/ember-api-docs-f260be3bbe602aac5e4bf94c38a4a8e5.js');
+self.importScripts('https://ember-api-docs-frontend-staging.global.ssl.fastly.net/assets/ember-api-docs-2b3a0ba993896c4eb30ab3a2b4bfbe8e.js');
 // self.importScripts('http://pureqml.com/qml.index.min.js');
