1fd907967a215b32535f1b936546939e7a643e4f
Alex Kanunnikov <lifeart92@gmail.com>
2019-05-26 20:42:10 +0300

    ember links update

--- docs/ember/ww2.js
+++ docs/ember/ww2.js
@@ -34,9 +34,9 @@ window.setTimeout = self.setTimeout;
 //   },time);
 // };
 
-self.importScripts('https://ember-api-docs-frontend-staging.global.ssl.fastly.net/assets/vendor-0b337e88c5b7a635a3281ba1f12d6048.js');
+self.importScripts('https://api.emberjs.com/assets/vendor-829cc0174ff3c9d56c6813fd722aafc0.js');
 // self.importScripts('https://emberobserver.com/assets/vendor-63fb928166ab18a0d8a59b4c0239d88b.js');
 var regeneratorRuntime = window.regeneratorRuntime;
 // self.importScripts('https://emberobserver.com/assets/ember-observer-6183d2e97f093c7a7b31ab8faeda96d6.js');
-self.importScripts('https://ember-api-docs-frontend-staging.global.ssl.fastly.net/assets/ember-api-docs-2b3a0ba993896c4eb30ab3a2b4bfbe8e.js');
+self.importScripts('https://api.emberjs.com/assets/ember-api-docs-f46418e6a3373d50c23cbb8751928ed5.js');
 // self.importScripts('http://pureqml.com/qml.index.min.js');
